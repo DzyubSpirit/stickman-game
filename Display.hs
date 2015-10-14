484c30023600f2c887157d878ed6b64cc622b061
Vlad <Dzyubavlad@gmail.com>
2015-10-14 18:05:27 +0300

    Divided code by files

--- Display.hs
+++ Display.hs
@@ -1,14 +1,23 @@
 module Display(display) where
 
 import Graphics.UI.GLUT
+import StickMan
 
 myPoints :: [(GLfloat, GLfloat, GLfloat)]
 myPoints = [(cos.angle$k, sin.angle$k,0) | k <- [1..12]]
      where angle k = 2*pi*k/12 
 
+vertex2f :: (GLfloat, GLfloat) -> IO ()
+vertex2f (x, y) = vertex $ Vertex3 x y 0 
+
+color3 :: (GLfloat, GLfloat, GLfloat) -> IO ()
+color3 (r, g, b) = color $ Color3 r g b
+
 display :: DisplayCallback
 display = do
 	clear [ColorBuffer]
-	renderPrimitive Points $
-	    mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+	renderPrimitive Lines $ do
+		color3(0, 0, 0)
+		vertex2f (-1, -0.8)
+		vertex2f (1, -0.8)
 	flush	
--- main.hs
+++ main.hs
@@ -9,7 +9,7 @@ main = do
     initialWindowSize $= Size 640 480
     _window <- createWindow "Hello, world!"
 
-    
+    clearColor $= Color4 1 1 1 1
 
     displayCallback $= display
     mainLoop
